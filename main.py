538878e6a2573396ee2e329e860aca426f82e193
Rishav-Raj-Sinha <rajrishav028@gmail.com>
2026-03-23 12:28:22 +0530

    improved summarize feature by adding a note selection menu

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-import typer
-import ollama_integration
-import transcript_pull
+from pathlib import Path
+
 import questionary
+import typer
+from rich.console import Console
+from rich.markdown import Markdown
+
+import ollama_integration
 import render
+import transcript_pull
 
 app = typer.Typer()
 
+
 @app.command()
 def generate(id: str):
-    transcipt = (transcript_pull.pull(id))
+    transcipt = transcript_pull.pull(id)
     ollama_integration.chat(transcipt)
+
 
 @app.command()
 def summarize():
-    note = questionary.path("select").ask()
-    ollama_integration.summarize_note(note)
+    #    note = questionary.path("select").ask()
+    vault = Path("/home/rishav/obsidian_notes")
+    notes = [str(f) for f in vault.glob("**/*.md")]
+    selected = questionary.select("Choose a note:", choices=notes).ask()
+    summary = str(ollama_integration.summarize_note(selected))
+    summary = Markdown(summary)
+    console = Console()
+    console.print(summary)
+
 
 @app.command()
 def hello():
-    render.render()    
+    render.render()
+
 
 if __name__ == "__main__":
     app()
